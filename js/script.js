9d319ce6b36f942c5b84997abf81e1911138c9da
zuzuzunta0 <kohsub469@gmail.com>
2021-10-23 14:38:18 +0900

    Fix setInterval

--- js/script.js
+++ js/script.js
@@ -13,4 +13,4 @@ function showClock() {
   const msg = "現在時刻は、" + nowHour + ":" + nowMin + ":" + nowSec + " です。";
   document.getElementById("showTime").innerHTML = msg;
 }
-setInterval('showClock()', 1000);
+setInterval(function(){showClock()}, 1000);
